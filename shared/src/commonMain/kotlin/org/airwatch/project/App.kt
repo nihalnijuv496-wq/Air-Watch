dcde83c66aaf81db1029dd318e6945d264cf5863
Nihal <nihalnijuv496@gmail.com>
2026-06-13 14:50:34 +0530

    feat: made a standard screen to work with

--- shared/src/commonMain/kotlin/org/airwatch/project/App.kt
+++ shared/src/commonMain/kotlin/org/airwatch/project/App.kt
@@ -1,28 +1,24 @@
 package org.airwatch.project
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import airwatch.shared.generated.resources.Res
-import airwatch.shared.generated.resources.compose_multiplatform
+import org.airwatch.project.screens.MenuScreen
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        var screen by remember { mutableStateOf("menu")}
+
+        when(screen)
+        {
+            "menu" -> MenuScreen()
+        }
 
     }
 }
---
+++ shared/src/commonMain/kotlin/org/airwatch/project/screens/MenuScreen.kt
@@ -0,0 +1,54 @@
+package org.airwatch.project.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import org.airwatch.project.Theme.RowDivider
+
+
+@Composable
+fun MenuScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+    )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = 0.17f)
+        )
+        {
+
+        }
+
+        RowDivider()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = 0.65f)
+        )
+        {
+
+        }
+
+        RowDivider()
+
+        LazyRow(
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        {
+
+        }
+    }
+}
